d22c76ab1a7609fd93f9d37fc50108feba4fccb1
Megamug <brianmathews1@gmail.com>
2018-05-19 21:21:44 -0500

    Initial sample cards

--- cards/deck.rb
+++ cards/deck.rb
@@ -1,6 +1,13 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+#locations
+locations_data = Squib.csv file: 'locations-data.csv'
+
+Squib::Deck.new cards: locations_data['title'].size, width: '3.5 in', height: '2.5 in', layout: 'locations-layout.yml' do
+  background color: 'white'
+
+  text str: locations_data['title'], layout: 'title'
+  text str: locations_data['description'], layout: 'description'
+
+  save_png
 end
